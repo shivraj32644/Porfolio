453b56ee462691a3fdf7968bbf5e76021ee9a9ba
shivraj2corpcare <shivraj.dangi@corpcare.co.in>
2024-02-21 15:07:20 +0530

    update projects

--- src/Pages/Project.jsx
+++ src/Pages/Project.jsx
@@ -38,42 +38,43 @@ export const Project = () => {
             >
               <div className={styles.left}>
                 <div className={styles.img}>
-                  <img src="https://i.imgur.com/lPI0qTc.png" alt="" />
+                  <img src="https://i.imgur.com/REfFyFn.png" alt="" />
                 </div>
               </div>
               <div className={styles.right}>
-                <h2 className={styles.projectTitle}>Myntra Clone</h2>
+                <h2 className={styles.projectTitle}>Investment And Analytics Platform</h2>
 
                 <p className={styles.projectDesc}>
-                  Build clone of myntra.com in 7 days along with a team 5
-                  members.
+                CorpCare Investment and Analytics is a platform built for HNIs,
+                UHNIs, and corporates to invest in mutual funds and they can track
+                their investment in mutual funds, bonds, fixed deposits, and
+                AIF/PMS.
                   <h3>Features</h3>
                   <ul style={{ marginLeft: "30px" }}>
-                    <li>Login</li>
-                    <li>Signup</li>
-                    <li>Cart Page</li>
-                    <li>Checkout Page</li>
+                    <li>Signin, Signup, KYC Verification, CAN Creation</li>
+                    <li>•Analytical Dashboard for Mutual Funds, Fixed Deposits, Bonds,AIF/PMS</li>
+                    <li>Mutual Funds Explore Page</li>
+                    <li>Investment Page for Mutual Funds, Fixed Deposits, Bonds</li>
+                    <li>User Profile section to update user details</li>
                   </ul>
-                  <h2><b><i> Tech Stack :</i></b> HTML | CSS | Javascript | Local Storage</h2>
+                  <h2><b><i> Tech Stack :</i></b> Next Js | Redux | Typecript | Tailwind CSS | React-Query
+                  | React-Hook-Form | Recharts | YUP</h2>
                   <h2>
-                   <b><i> Area of Responsibility :</i></b> Developed the product page and
-                    navigation bar
+                   <b><i> Area of Responsibility :</i></b>  My role in the project involves constructing
+                      responsive UI based on Figma designs, seamlessly integrating APIs
+                      across all pages. I consistently implement platform modifications in
+                      alignment with evolving business requirements.
+
                   </h2>
                 </p>
                 <div className={styles.buttons}>
+                
                   <a
-                    href="https://github.com/shivraj32644/MyntraClone"
+                    href="https://client.corpcare.co.in/signin"
                     target="_blank"
                     className={styles.primaryBtn}
                   >
-                    GitHub Link
-                  </a>
-                  <a
-                    href="https://myntracom.netlify.app/"
-                    target="_blank"
-                    className={styles.primaryBtn}
-                  >
-                    Preview{" "}
+                    Live Link{" "}
                   </a>
                 </div>
               </div>
@@ -93,110 +94,49 @@ export const Project = () => {
             >
               <div className={styles.left}>
                 <div className={styles.img}>
-                  <img src="https://i.imgur.com/2QNF7AG.png" alt="" />
+                  <img src="https://raw.githubusercontent.com/shivraj32644/Images/main/website-cc.png?token=GHSAT0AAAAAACKUC7MYWW72VZHJYVXK33D4ZOVYOPA" alt="" />
                 </div>
               </div>
               <div className={styles.right}>
                 <h2 className={styles.projectTitle}>HappyFox Clone</h2>
 
                 <p className={styles.projectDesc}>
-                  Build clone of HappyFox.com in 5 days along with a team of 5
-                  members. HappyFox is a (SaaS) support platform. That offer an
-                  enterprise-grade help desk ticketing system.
+                This website serves as a lending platform for CorpCare, allowing
+                users to explore the services offered by CorpCare.
+
                   <h2>Features</h2>
                   <ul style={{ marginLeft: "30px" }}>
-                    <li>Login</li>
-                    <li>Signup</li>
-                    <li>Admin Panel</li>
-                    <li>Ticket System</li>
+                    <li>Home Page, Services Pages, Products Pages</li>
+                    <li>About Company, Research Pages</li>
                   </ul>
                   <h2>
-                   <b><i>Tech Stack :</i></b>  HTML | CSS | Javascript | JSON-Server | API{" "}
+                   <b><i>Tech Stack :</i></b>  AstroJs | React |Tailwind CSS | Typescript | Strapi CMS
                   </h2>
                   <h2>
-                   <b><i>Area of Responsibility :</i></b>  Contributed to the creation of a ticket system and a
-                    navigation bar.
+                   <b><i>Area of Responsibility :</i></b>  Previously, this project was built using
+                  Next.js and Strapi, which was developed by the previous team. They
+                  overengineered the project with a heavy dependency on Strapi.
+                  However, since this is a static website, it does not require such a
+                  significant reliance on Strapi. Therefore, I rebuilt the website using
+                  Astro.js, and only the blogs and research paper sections are being
+                  sourced from Strapi.
+
                   </h2>
                 </p>
                 <div className={styles.buttons}>
+                  
                   <a
-                    href="https://github.com/Vinay-Khairnar/HappyFox-Clone"
+                    href="https://corpcare.co.in/"
                     target="_blank"
                     className={styles.primaryBtn}
                   >
-                    Github Link
-                  </a>
-                  <a
-                    href="https://idyllic-scone-5f37c8.netlify.app/"
-                    target="_blank"
-                    className={styles.primaryBtn}
-                  >
-                    Preview{" "}
+                    Live Link{" "}
                   </a>
                 </div>
               </div>
             </motion.div>
             {/* ========================================================================================================================================= */}
 
-            <motion.div
-              className={styles.item}
-              initial={{ x: 70, opacity: 0 }}
-              whileInView={{ x: 0, opacity: 1 }}
-              viewport={{ once: false, amount: 0.1 }}
-              transition={{
-                duration: 3.5,
-                type: "spring",
-                stiffness: 300,
-              }}
-              // transition={{ delay: 0.1 }}
-            >
-              <div className={styles.left}>
-                <div className={styles.img}>
-                  <img src={jiomartImg} alt="olx" />
-                </div>
-              </div>
-              <div className={styles.right}>
-                <h2 className={styles.projectTitle}>Jiomart Clone</h2>
-
-                <p className={styles.projectDesc}>
-                  Build full stack clone of jiomart.com in 6 days along with a
-                  team of 4 members. Jiomart is an e-commerce platform where
-                  user can buy products
-                  <h2>Features</h2>
-                  <ul style={{ marginLeft: "30px" }}>
-                    <li>Login</li>
-                    <li>Signup</li>
-                    <li>Product Pages</li>
-                    <li>Cart Page</li>
-                  </ul>
-                  <h2>
-                    <b> <i> Tech Stack : </i></b> Node js | Express | MongoDB | React | Redux |
-                    Routing | Chakra-UI |
-                  </h2>
-                  <h2>
-                   <b> <i> Area of Responsibility :</i></b> Build product API along with sort and filter functionality
-                    and also worked on connecting the frontend with the backend.
-                  </h2>
-                </p>
-
-                <div className={styles.buttons}>
-                  <a
-                    href="https://github.com/shivraj32644/JioMart-FullStack"
-                    target="_blank"
-                    className={styles.primaryBtn}
-                  >
-                    Github Link
-                  </a>
-                  <a
-                    href="https://jiomart-fullstack.netlify.app/"
-                    target="_blank"
-                    className={styles.primaryBtn}
-                  >
-                    Preview{" "}
-                  </a>
-                </div>
-              </div>
-            </motion.div>
           </div>
         </div>
       </section>
